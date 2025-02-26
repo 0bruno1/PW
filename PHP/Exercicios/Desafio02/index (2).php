2264191d2778f37727ac34892b6d9d8df847e234
BrunoMendes/Bruno <bruninhu121nopudim@gmail.com>
2025-02-26 20:27:17 -0300

    index.php

--- PHP/Exercicios/Desafio02/index (2).php
+++ PHP/Exercicios/Desafio02/index (2).php
@@ -7,47 +7,47 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-   <div class="php">
+<div class="php">
     <div class="container">
         <?php
                 $altura = 1;
-                $peso = 1;
+                $peso = 25;
                 $imc = $peso /($altura * $altura);
                 if($imc < 18.5){
-                    echo "<p >" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc" . " O que significa que está em excesso de magreza.";
-                    echo "<img class ='magreza' ";
+                    echo "<img src='images/magreza.jpg' class ='magreza' 100px 300px> ";
                 }
                 else if($imc <= 24.9 ){
-                    echo "<p>" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc " . " O que significa que seu peso está normal de acordo com sua estatura.";
-                    echo "<img class ='pesoNormal'>";
+                    echo "<img src='images/normal.jpg' class ='magreza' 100px 300px> ";
                 }
                 else if($imc < 29.9 ){
-                    echo "<p>" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc " . " O que significa que está em excesso de peso.";
-                    echo "<img class ='acimaDoPeso'>";
+                    echo "<img src='images/sobrepeso.jpg' class ='magreza' 100px 300px> ";
                 }
                 else if($imc <= 34.9 ){
-                    echo "<p>" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc " . " O que significa que está em Obesidade I.";
-                    echo "<img class ='obesidadeI'>";
+                    echo "<img src='images/obesidade1.jpg' class ='magreza' 100px 300px> ";
                 }
                 else if($imc < 39.9 ){
-                    echo "<p>" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc " . " O que significa que está em Obesidade II.";
-                    echo "<img class ='obesidadeII'>";
+                    echo "<img src='images/obesidade2.png' class ='magreza' 100px 300px> ";
                 }
                 else{
-                    echo "<p>" . "você tem $altura de altura" . "</p>";
+                    echo "<p>" . "você tem $altura m de altura" . "</p>";
                     echo "<p>" ."Voce possui $peso Kg". "</p>";
                     echo "<p> Seu IMC é de: $imc " . " O que significa que está em  Obesidade III.";
-                    echo "<img class ='obesidadeIII'>";
+                    echo "<img src='images/magreza.jpg' class ='magreza' 100px 300px> ";
                 }  
             ?>
         </div>
