c362d8ba8758293614a9d6b2f636d0cf500fb7a4
BrunoMendes/Bruno <bruninhu121nopudim@gmail.com>
2025-05-28 18:38:31 -0300

    Update lista.php

--- ProjetoEcommerce/lista.php
+++ ProjetoEcommerce/lista.php
@@ -1,2 +1,39 @@
+    
+<div class="containerTabela">
+    <h1 class="TituloRequisitos">Requisitos funcionais<br> do <br> nosso projeto.</h1>
+    <section class="seçãoTabelaRequisitos">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">número</th>
+                <th scope="col">Requisito Funcional</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <th scope="row">1</th>
+                    <td>O projeto deve apresentar os produtos para compra</td>
+                </tr>
+                        
+                <tr>
+                    <th scope="row">2</th>
+                    <td>O projeto deve permitir a busca por produtos</td>
+                </tr>
+                        
+                <tr>
+                    <th scope="row">3</th>
+                    <td>O projeto deve permitir a filtração de produtos por preço</td>
+                </tr>
+
+                <tr>
+                    <th scope="row">4</th>
+                    <td>O projeto deve permitir a instalação de cookies para recomendação de produtos</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</div>
+
+
 
 
