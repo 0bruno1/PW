23efab36285b2c79fbfe130cfd8ff0c72f1752d8
BrunoMendes/Bruno <bruninhu121nopudim@gmail.com>
2025-05-28 21:19:17 -0300

    Add files via upload

---
+++ ProjetoEcommerce/footer.php
@@ -0,0 +1,55 @@
+
+<link rel="stylesheet" href="CSS/footer.css">
+
+<footer>
+  <div class="footer-top">
+    <div class="social">
+      <h4>Seguir a Galaxy</h4>
+      <div class="icons">
+        <a href="#"><img src="ASSETS/facebook.png"></a>
+        <a href="#"><img src="ASSETS/instagram.png" alt="Instagram"></a>
+        <a href="#"><img src="ASSETS/twitter-alt-square.png" alt="X"></a>
+        <a href="#"><img src="ASSETS/youtube.png" alt="YouTube"></a>
+        <a href="#"><img src="ASSETS/twitch.png" alt="Twitch"></a>
+        <a href="#"><img src="ASSETS/discord.png" alt="Discord"></a>
+        <a href="#"><img src="ASSETS/linkedin.png" alt="LinkedIn"></a>
+      </div>
+    </div>
+
+    <div class="footer-links">
+      <div class="column">
+        <h4>Institucional</h4>
+        <ul>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Formulário</a></li>
+          <li><a href="#">Segurança</a></li>
+        </ul>
+      </div>
+      <div class="column">
+        <h4>Ajuda</h4>
+        <ul>
+          <li><a href="#">Suporte</a></li>
+          <li><a href="#">Termos de Uso</a></li>
+          <li><a href="#">Política de Privacidade</a></li>
+          <li><a href="#">Procon</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="language-country">
+      <p>Português | Brasil</p>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <div class="logo">
+      <img src="assets/logogalaxy.png" alt="Logo">
+      <p>© 2011 - 2025</p>
+    </div>
+    <div class="address">
+      <p>Galaxy Ltda. – CNPJ 00.000.000/0001-10</p>
+      <p>Rua Danilo Braga, nº 1610, sala 666 - Torre do Sul - Guaianases - São Paulo, SP - 161020-008</p>
+    </div>
+  </div>
+</footer>
